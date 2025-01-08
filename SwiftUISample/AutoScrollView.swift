eed953dcf36c8f389905c703526d459537943970
marha-hwang <hwangharam@naver.com>
2025-01-09 00:14:20 +0900

    feat. AutoScroll구현

--- SwiftUISample/AutoScrollView.swift
+++ SwiftUISample/AutoScrollView.swift
@@ -11,65 +11,72 @@ import SwiftUI
 /**
  컴포넌트 설명 : 상단에 탭버튼이 존재하고 사용자가 스크롤시 지정된 위치로 이동하는 컴포넌트
  컴포넌트 구조 :
+ scrollView Indicator커스텀 필요 https://luthviar.medium.com/swiftui-custom-scrollview-indicator-horizontally-7f4fe2e20bc9
  */
 struct AutoScrollView: View {
-    @State private var scrollOffset: CGFloat = 0
-    @State private var scrollTimer: Timer? = nil
-    @State private var isScrolling: Bool = false
-    var imageArr = ["food1", "food2", "food3"]
+    
+    let menulist = ["인기메뉴", "[갓성비] 후라이드 콤보세트", "[한정판메] 큐브 스테이크 콤보세트", "[사이드] 음료 및 스낵"]
     
     var body: some View {
-        ScrollViewReader{ proxy in
-                ScrollView(.horizontal){
-                    GeometryReader{ geo in
-                        HStack(spacing:0){
-                            ForEach(imageArr, id: \.self) { image in
-                                GeometryReader{geo in
-                                    Image(image)
-                                        .resizable()
-                                        .frame(width: UIScreen.main.bounds.width, height: 300)
-                                        .scaledToFit()
-                                        .id(image)
-                                }
-                            }
-                        }
-                        .onAppear {
-                            // 초기 위치
-                            print("스크롤 시작 위치: \(geo.frame(in: .global).minX)")
-                        }
-                        .onChange(of: geo.frame(in: .global).minX) { newMinX in
-                            // 스크롤 이동 시마다 오프셋 업데이트
-                            scrollOffset = -newMinX
-                            startScrollDetection(proxy: proxy)
-                            print("현재 스크롤 오프셋: \(scrollOffset)")
-                        }
-                }
-                .frame(width: UIScreen.main.bounds.width*CGFloat(imageArr.count), height: 300)
+        VStack(spacing: 0){
+            UnderLineScrollView(menulist:menulist){
+                CustomText(text:"메뉴 선택")
             }
+
+            .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+            .background(Color.blue)
+            
+            ScrollView{
+                
+            }.background(Color.yellow)
         }
     }
     
-    // 스크롤 멈춤 감지 함수
-    private func startScrollDetection(proxy:ScrollViewProxy) {
-        // 이전 타이머가 존재하면 취소
-        scrollTimer?.invalidate()
+    /*
+     @ViewBuilder : View를 반환하는 클로저 앞에 @ViewBuilder을 붙힘으로써 해당 클로저는 뷰를 생성할 수 있게 된다.
+         body 클로저 변수는 암시적으로 @ViewBuilder가 붙어있기 때문에 별도로 추가하지 않아도 뷰를 생성 할 수 있는 것!
+     
+     
+     */
+    struct UnderLineScrollView<Content:View>:View{
+        private let content:()->Content
+        private let menulist:[String]
         
-        // 새로운 타이머 설정
-        scrollTimer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: false) { _ in
-            // 일정 시간 후 스크롤이 멈췄다고 간주
-            withAnimation {
-                isScrolling = false
-            }
-            
-            var target = Int(scrollOffset / (UIScreen.main.bounds.width/2))
-            if(target >= imageArr.count) {target = imageArr.count-1}
-            proxy.scrollTo(imageArr[target])
-            print("스크롤이 멈췄습니다.")
+        init(menulist:[String], @ViewBuilder content: @escaping () -> Content) {
+            self.content = content
+            self.menulist = menulist
         }
         
-        // 스크롤이 움직일 때마다 isScrolling을 true로 설정
-        isScrolling = true
+        var body: some View{
+            ScrollViewReader{ proxy in
+                content()
+                ScrollView(.horizontal){
+                    HStack{
+                        var i = 0
+                        ForEach(0..<menulist.count-1, id: \.self) { i in
+                            CustomText(text: menulist[i])
+                                .onTapGesture {
+                                    proxy.scrollTo(i-1 < 0 ? 0:i-1, anchor: .leading)
+                                }
+                                .id(i)
+                            if i != menulist.count-1 { Spacer(minLength: 30)}
+                           
+                        }
+                    }
+                }
+            }
+        }
+        
     }
+    
+    struct CustomText:View{
+        var text:String
+        var body:some View{
+            Text(text)
+                .font(.custom("BMHANNAPro", size: 20))
+        }
+    }
+        
 }
 
 #Preview {
--- SwiftUISample/SwiftUIView.swift
+++ SwiftUISample/SwiftUIView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("[치킨콤보] 우리모두 즐겁게 먹어요")
+            .font(.custom("Helvetica Neue", size: 20))
+            .fontWeight(.bold)
+            .foregroundColor(.red)
+            .kerning(23)
+            .lineSpacing(30)
+            .multilineTextAlignment(.center)
+            .shadow(color: .black, radius: 5, x: 3, y: 3)
+            .frame(width:UIScreen.main.bounds.width, height: 300, alignment: .top)
+            .background(Color.green)
     }
 }
 
